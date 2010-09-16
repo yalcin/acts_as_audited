a7d6a2c5a16cc24b7e0f082c9b8c6fb535f1c496
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-09-16 10:59:11 -0700

    switch to acts_as_audited_rails3

---
+++ lib/acts_as_audited_rails3.rb
@@ -0,0 +1,9 @@
+require 'acts_as_audited_rails3/base'
+require 'acts_as_audited_rails3/audit'
+require 'acts_as_audited_rails3'
+
+ActiveRecord::Base.send :include, CollectiveIdea::Acts::Audited
+
+if defined?(ActionController) and defined?(ActionController::Base)
+  require 'acts_as_audited_rails3/audit_sweeper'
+end
